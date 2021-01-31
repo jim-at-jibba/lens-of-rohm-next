952af88df043a73855e673677171bec3381766a0
James Best <james.best@candide.eu>
2021-01-31 05:10:01 +0000

    Add anchor tag hover styles

--- theme/globalStyles.ts
+++ theme/globalStyles.ts
@@ -3,6 +3,12 @@ import {createGlobalStyle} from "styled-components"
 const GlobalStyle = createGlobalStyle`
   a {
     text-decoration: none;
+    color: black;
+    transition: color 500ms;
+
+    &:hover {
+      color: lightgrey
+    }
   }
 `
 
